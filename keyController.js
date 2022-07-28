bb44c0bb40cbd3a1312807ecbcccd86593955226
SamuelKasper <Samuel.Noah.Kasper@hs-furtwangen.de>
2022-07-28 14:19:33 +0200

    finished v1

--- keyController.js
+++ keyController.js
@@ -1,15 +1,15 @@
 document.addEventListener('keypress', (key) => {
     if(key.key == "d"){
-        document.getElementById("btn1").style.backgroundColor = "red";
+        document.getElementById("btn1").style.backgroundColor = "gray";
  
     }else if(key.key == "f"){
-        document.getElementById("btn2").style.backgroundColor = "red";
+        document.getElementById("btn2").style.backgroundColor = "gray";
     
     }else if(key.key == "j"){
-        document.getElementById("btn3").style.backgroundColor = "red";
+        document.getElementById("btn3").style.backgroundColor = "gray";
  
     }else if(key.key == "k"){
-        document.getElementById("btn4").style.backgroundColor = "red";
+        document.getElementById("btn4").style.backgroundColor = "gray";
     }
 });
 
@@ -19,3 +19,7 @@ document.addEventListener('keyup', (key) =>{
     document.getElementById("btn3").style.backgroundColor = "white";
     document.getElementById("btn4").style.backgroundColor = "white";
 });
+
+function checkHit(){
+
+}
--- noteMapping.js
+++ noteMapping.js
@@ -1,4 +1,15 @@
+//combo and score
+let combo = 0;
+let score = 0;
+
 function startGame() {
+    //config variables
+    let noteAmount = 75;
+    // must both be multiplied with 795 a multiple of 400ms.
+    let delayBetweenNotes = 800;
+    //possible values: 1.0062, 1.51, 2.0126
+    let delayNoteMovement = 2.0126;
+
     //hide start button
     document.getElementById("start").style.display = "none";
 
@@ -9,30 +20,75 @@ function startGame() {
     //start audio (Arcade got 75 Bpm! Therefor create every 1.25s a new Note)
     const audio = new Audio("Music_Arcade.mp3");
     audio.play();
-
-    //create notes
-    let newNote = document.createElement("div");
-    newNote.className = "noteStyle noteJ";
-    newNote.id = "note0";
-    document.getElementById("background").appendChild(newNote);
-    moveNotes(newNote)
+    noteHandler(noteAmount, delayBetweenNotes, delayNoteMovement);
 }
 
-//function for moving the notes
-async function moveNotes(newNote) {
-    let objStyle = window.getComputedStyle(newNote);
-    let topValue = objStyle.getPropertyValue("top").replace("px", "");
-
-    for(let i = newNote.style.top; i<750; i++){
-        newNote.style.top  = i + "px";
-        await delay(10);
+//create new notes with specific delay
+async function noteHandler(noteAmount, delayBetweenNotes, delayNoteMovement) {
+    for (let i = 0; i < noteAmount; i++) {
+        //create random value 0-3 to decide on which lane the note should spawn
+        let rnd = Math.floor(Math.random() * 4);
+        let noteLane = "";
+        if (rnd == 1) {
+            noteLane = "noteF";
+        } else if (rnd == 2) {
+            noteLane = "noteJ";
+        } else if (rnd == 3) {
+            noteLane = "noteK";
+        }
+        //create notes
+        let newNote = document.createElement("div");
+        newNote.className = `noteStyle ${noteLane}`;
+        newNote.id = `note${i}`;
+        document.getElementById("background").appendChild(newNote);
+        moveNotes(newNote, delayNoteMovement)
+        //delay has to set for every song specific
+        await delay(delayBetweenNotes);
     }
-    document.getElementById("background").removeChild(newNote);
+}
+
+//function for moving the notes and check if a note is hit
+async function moveNotes(newNote, delayNoteMovement) {
+    let removed = false;
+    for (let i = newNote.style.top; i < 800; i++) {
+        //check if note is hit
+        if (i > 775) {
+            let obj;
+            let classString = newNote.className + "";
+            if (classString.includes("noteF")) {
+                obj = document.getElementById("btn2");
+            } else if (newNote.className.includes("noteJ")) {
+                obj = document.getElementById("btn3");
+            } else if (newNote.className.includes("noteK")) {
+                obj = document.getElementById("btn4");
+            } else { obj = document.getElementById("btn1"); }
+
+            //check if hit and update score and combo
+            let style = getComputedStyle(obj);
+            if (style["background-color"] == "rgb(128, 128, 128)") {
+                combo = combo + 1;
+                score = score + 10 * combo;
+                document.getElementById("combo").innerHTML = `Combo: ${combo}`;
+                document.getElementById("score").innerHTML = `Score: ${score}`;
+                document.getElementById("background").removeChild(newNote);
+                removed = true;
+                break;
+            }
+        }
+        //move note and wait
+        await delay(delayNoteMovement);
+        newNote.style.top = i + "px";
+    }
+    if (!removed) {
+        document.getElementById("background").removeChild(newNote);
+        combo = 0;
+        document.getElementById("combo").innerHTML = `Combo: ${combo}`;
+    }
 }
 
 // delay for moving
-function delay(n){
-    return new Promise(function(resolve){
-        setTimeout(resolve,n);
+function delay(n) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, n);
     });
 }
